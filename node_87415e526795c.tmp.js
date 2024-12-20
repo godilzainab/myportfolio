f8deeea0e4b64a2978ef667a8f54c6c713f637fb
Zainab Godil <zainabgodil23@gmail.com>
2024-12-20 13:27:37 +0530

    add

---
+++ node_87415e526795c.tmp.js
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shezan outlet</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div id="navbar">
+        <div id="name">
+            <h1>Portfolio</h1>
+        </div>
+        <div id="listitem">
+            <a href="#i"> Home </a>
+            <a href="#aboutMe">About </a>
+            <a href="#c"> Projects</a>
+            <a href="#contact"> Contact</a>
+        </div>
+    </div>
+    <div>
+
+        <div class="padding">
+            <img src="image/banner.jpg" class="hero-img" id="i">
+        </div>
+
+        <div class="flex">
+            <img src="image/mypic.jpeg" class="mypic1" id="aboutMe">
+            <div class="about-para">
+                <p class="styling">So,Who am I?</p>
+                <p class="para">As an aspiring <span class="clor">Frontend Developer and Graphic Designer</span>, I am
+                    passionate about creating visually
+                    appealing and user-friendly digital experiences. With a solid understanding of HTML, CSS, and
+                    JavaScript, I am eager to expand my skills and grow as a developer. On the design side, I enjoy
+                    using graphic design tools to create engaging visuals and user interfaces that reflect
+                    creativity and clarity. Although I am just starting my career, I am motivated to learn,
+                    collaborate, and bring my ideas to life in both web development and design projects.
+
+                </p>
+            </div> 
+        </div>
+        <h1 class="center" id="c"> Projects </h1>
+        <div class="div-color">
+            <div class="projects-1">
+                <h3>Study-Website of Amazon</h3>
+
+                <p>I created the front page of the Amazon website for my portfolio using only HTML and CSS, focusing on
+                    responsive design and clean layout. This project demonstrates my ability to replicate real-world web
+                    designs with attention to detail and user experience.</p>
+                    <a href="https://godilzainab.github.io/studyweb/" class="black">Click here to see my work </a>
+            </div>
+
+
+            <div class="projects-2">
+                <h3>Study-Website of Vegan Kitchen</h3>
+
+                <p>
+                    I built a Vegan Kitchen front-page website as a study project using HTML, CSS, and JavaScript, focusing on
+                    creating an engaging and user-friendly design. The site highlights my skills in web development,
+                    including responsive layout and interactive features for a seamless user experience..</p>
+                    <a href="https://godilzainab.github.io/studyvegan/" class="black">Click here to see my work </a>
+            </div>
+
+           
+        </div>
+        <div id="contact">
+            <p class="contactline"> Feel free to reach me on <a>LinkedIn or Gmail</a></p>
+            <img src="image/vintage-style-brass---wood-dummy-retro-telephone-by-exim-decor-vintage-style-brass---wood-dummy-retr-zvquoz.jpg"
+                class="phn">
+        </div>
+
+        <div id="footer">
+            <h3>zainabgodil</h3>
+        </div>
+        <script src=".js">
+        </script>
+        <script src="https://kit.fontawesome.com/9cf61a0330.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
